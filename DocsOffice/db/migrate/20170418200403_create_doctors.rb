5a4660a389059b06628b4eaf04109222b22af6fa
Swapna Brungi <swapna.brungi@gmail.com>
2017-04-18 17:18:48 -0400

    finished migrations for DocsOffice

--- DocsOffice/db/migrate/20170418200403_create_doctors.rb
+++ DocsOffice/db/migrate/20170418200403_create_doctors.rb
@@ -2,7 +2,7 @@ class CreateDoctors < ActiveRecord::Migration[5.0]
   def change
     create_table :doctors do |t|
       t.string :name
-      t.integer :phone_number
+      t.integer :phone_number 
 
       t.timestamps
     end
